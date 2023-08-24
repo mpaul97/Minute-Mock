74a473886d19c01198626fe3f3a3244f37178146
mpaul97 <michaelandrewpaul97@gmail.com>
2023-08-24 16:26:27 -0500

    computer ai

---
+++ src/assets/computerAi/computerAi.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import os
+
+class ComputerAi:
+    def __init__(self, _dir):
+        self._dir = _dir
+        return
+    def buildTrain(self):
+        
+        return
+    
+# / END ComputerAi
+
+##########################
+
+ca = ComputerAi("./")
+
+# train - round, current team, available players
